66f5fa40604c97055b32194950fdd80708a6faa3
Michael Vasquez <michael.a.vasquez.gm@gmail.com>
2024-06-09 23:49:03 -0400

    Add out functions and ease out

--- pkg/Sprite.animation.go
+++ pkg/Sprite.animation.go
@@ -12,9 +12,10 @@ type spriteAnimType int
 type spriteAnimState int
 
 const (
-	spriteAnimType_NONE    spriteAnimType = 0
-	spriteAnimType_LINEAR  spriteAnimType = 1
-	spriteAnimType_EASE_IN spriteAnimType = 2
+	spriteAnimType_NONE     spriteAnimType = 0
+	spriteAnimType_LINEAR   spriteAnimType = 1
+	spriteAnimType_EASE_IN  spriteAnimType = 2
+	spriteAnimType_EASE_OUT spriteAnimType = 3
 
 	spriteAnimState_NONE        spriteAnimState = -1
 	spriteAnimState_INIT        spriteAnimState = 0
@@ -22,11 +23,11 @@ const (
 	spriteAnimState_COMPLETE    spriteAnimState = 2
 )
 
-func (sprite *Sprite) SetEaseIn(totalTime time.Duration, startDelta rl.Vector2) {
+func (sprite *Sprite) setupAnimation(sAnimType spriteAnimType, totalTime time.Duration, startDelta rl.Vector2) {
 	sprite.animationTime = time.Duration(0)
 	sprite.animationDuration = totalTime
 
-	sprite.animationType = spriteAnimType_EASE_IN
+	sprite.animationType = sAnimType
 	sprite.animationState = spriteAnimState_INIT
 
 	startVector := rl.Vector2Add(sprite.GetWindowPosition(), startDelta)
@@ -37,17 +38,14 @@ func (sprite *Sprite) SetEaseIn(totalTime time.Duration, startDelta rl.Vector2) 
 }
 
 func (sprite *Sprite) SetLinear(totalTime time.Duration, startDelta rl.Vector2) {
-	sprite.animationTime = time.Duration(0)
-	sprite.animationDuration = totalTime
+	sprite.setupAnimation(spriteAnimType_LINEAR, totalTime, startDelta)
+}
 
-	sprite.animationType = spriteAnimType_LINEAR
-	sprite.animationState = spriteAnimState_INIT
-
-	startVector := rl.Vector2Add(sprite.GetWindowPosition(), startDelta)
-	dest := sprite.GetWindowDestRect()
-
-	sprite.animationStartRect = rl.NewRectangle(startVector.X, startVector.Y, dest.Width, dest.Height)
-	sprite.animationProgressRect = sprite.animationStartRect
+func (sprite *Sprite) SetEaseIn(totalTime time.Duration, startDelta rl.Vector2) {
+	sprite.setupAnimation(spriteAnimType_EASE_IN, totalTime, startDelta)
+}
+func (sprite *Sprite) SetEaseOut(totalTime time.Duration, startDelta rl.Vector2) {
+	sprite.setupAnimation(spriteAnimType_EASE_OUT, totalTime, startDelta)
 }
 
 func (sprite *Sprite) getAnimationDestRect() rl.Rectangle {
@@ -75,6 +73,8 @@ func (sprite *Sprite) updateAnimations(deltaTime time.Duration) {
 		progress = sprite.getLinearProgress(timeFraction)
 	case spriteAnimType_EASE_IN:
 		progress = sprite.getEaseInProgress(timeFraction)
+	case spriteAnimType_EASE_OUT:
+		progress = sprite.getEaseOutProgress(timeFraction)
 	}
 
 	destRect := sprite.GetWindowDestRect()
@@ -96,6 +96,10 @@ func (sprite *Sprite) getEaseInProgress(timeFraction float64) float64 {
 	return timingQuad(timeFraction)
 }
 
+func (sprite *Sprite) getEaseOutProgress(timeFraction float64) float64 {
+	return asOutFunction(timingQuad)(timeFraction)
+}
+
 // https://javascript.info/js-animation
 
 func timingLinear(in float64) float64 {
@@ -103,4 +107,22 @@ func timingLinear(in float64) float64 {
 }
 func timingQuad(in float64) float64 {
 	return tools.RoundFloat(math.Pow(in, 2), 4)
+}
+
+// func timingBounce(in float64) float64 {
+// 	var a float64 = 0
+// 	var b float64 = 1
+
+// 	for {
+// 		if in >= (7-4*a)/11 {
+// 			return -math.Pow((11-6*a-11*in)/4, 2) + math.Pow(b, 2)
+// 		}
+// 	}
+// }
+
+// accepts a timing function, returns the transformed variant
+func asOutFunction(timing func(float64) float64) func(float64) float64 {
+	return func(timeFraction float64) float64 {
+		return 1 - timing(1-timeFraction)
+	}
 }
