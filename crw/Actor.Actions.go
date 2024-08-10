3b178a12731a9d3511f47375ebb5dd0e57eec140
Michael Vasquez <michael.a.vasquez.gm@gmail.com>
2024-08-10 02:14:25 -0400

    Remove debug statements

--- crw/Actor.Actions.go
+++ crw/Actor.Actions.go
@@ -1,7 +1,6 @@
 package crw
 
 import (
-	"crowform/internal/mog"
 	"crowform/internal/tools"
 	"sort"
 	"time"
@@ -33,36 +32,27 @@ func (actor *Actor) runActions(deltaTime time.Duration) {
 		return
 	}
 
-	mog.Debug("%v -- runActions::actions length = %d", actor.Tags, len(actions))
-	mog.Debug("%v -- runActions::actions slice = %v", actor.Tags, actions)
-
 	sort.Slice(actions, func(i, j int) bool {
 		return actions[i].index < actions[j].index
 	})
-	mog.Debug("%v -- runActions::actions sorted = %v", actor.Tags, actions)
 
 	actor.doActions(deltaTime, actions, 0, func() {
-		mog.Debug("%v -- runActions::SetAsReady", actor.Tags)
 		actor.ActionsSetAsReady()
 	})
 }
 
 func (actor *Actor) doActions(deltaTime time.Duration, allActions []ActorAction, idx int, onComplete func()) {
 	if idx > len(allActions)-1 {
-		mog.Debug("%v -- doActions:: onComplete called idx = %d, len = %d", actor.Tags, idx, len(allActions))
 		onComplete()
 		return
 	}
 
 	// Check if still valid when - can change based on other actions run
 	if allActions[idx].when(actor) {
-		mog.Debug("%v -- doActions:: when TRUE idx = %d", actor.Tags, idx)
 		allActions[idx].do(deltaTime, actor, func() {
-			mog.Debug("%v -- doActions:: when TRUE done called for idx = %d", actor.Tags, idx)
 			actor.doActions(deltaTime, allActions, idx+1, onComplete)
 		})
 	} else {
-		mog.Debug("%v -- doActions:: when FALSE idx = %d", actor.Tags, idx)
 		actor.doActions(deltaTime, allActions, idx+1, onComplete)
 	}
 }
